3be37818b70640a5f19b009b49fb2b248d8d7b9d
JustinEckard <justinweckard@gmail.com>
2024-08-15 19:21:53 +0200

    Working on day 6, routes and passing things to templates with props and attributes

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,4 @@
+@props(['active' => false, 'type'=>'anchor'])
+
+
+<a {{ $attributes }} class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md   hover:bg-gray-700 px-3 py-2 text-sm font-medium text-white" aria-current="page">{{ $slot }}</a>
